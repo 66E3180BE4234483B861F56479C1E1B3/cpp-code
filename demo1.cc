c3349c6f1a190d5c75f30841a770f1dee0deef3b
yuri-hu <YuriHuGL@outlook.com>
2020-03-22 11:20:18 +0800

    Sequential List v0.2
    Resolve #2
    
    Signed-off-by: yuri-hu <YuriHuGL@outlook.com>

--- demo1.cc
+++ demo1.cc
@@ -1,11 +1,44 @@
+#include <iostream>
+
 #include "gtest/gtest.h"
+#include "seqList-v0.2.h"
 
 TEST(Suite1, Test1) {
-  int res = 1 + 2;
-  EXPECT_EQ(res, 3);
+    SeqList<int> seqList(3);
+    const int N = 3;
+
+    for (int i = 0; i < N; i++) {
+        seqList.insert(i + 1);
+    }
+
+    int n = 1;
+    for (int d : seqList) {
+        EXPECT_EQ(d, n);
+        n++;
+    }
+    EXPECT_EQ(n, N + 1);
+}
+
+template <typename T>
+void func(T n)
+{
+    std::cout << n << " ";
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    SeqList<char> l(10);
+    for (int i = 0; i < 10; i++)
+        l.insert(i + 'a');
+    l.traversal([] (char d) {std::cout << d << " ";});
+    std::cout << std::endl;
+
+    for (int i = 0; i < 10; i++)
+        std::cout << l.remove(0) << " ";
+
+    l.traversal(func<char>);
+    std::cout << std::endl;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
+
